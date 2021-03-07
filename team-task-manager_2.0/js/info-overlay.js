fde4d468c86e2b905e032830eb7686e45b8b786e
Fredrik Skogstad <52358347+Fredrik9400@users.noreply.github.com>
2021-03-07 14:32:56 +0100

    Delete team-task-manager_2.0 directory

--- team-task-manager_2.0/js/info-overlay.js
+++
@@ -1,6 +0,0 @@
-function turnOnOverlay() {
-  document.getElementById("overlay").style.display = "block";
-}
-function turnOffOverlay() {
-  document.getElementById("overlay").style.display = "none";
-}
